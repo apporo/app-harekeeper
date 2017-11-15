70035628b0a33b615600a474e606a819c18ef5e5
pnhung177 <pnhung177@acegik.net>
2017-11-15 18:04:26 +0700

    Add recyclers

---
+++ test/app/README.md
@@ -0,0 +1,7 @@
+# app-harekeeper
+
+## Usage
+
+```shell
+export OPFLOW_LAB_URI=amqp://master:zaq123edcx@192.168.56.56
+```
--- test/app/config/sandbox.js
+++ test/app/config/sandbox.js
@@ -1,7 +1,15 @@
 module.exports = {
   plugins: {
     appHarekeeper: {
-      contextPath: '/harekeeper'
+      contextPath: '/harekeeper',
+      recycler: {
+        reportAPI: {
+          uri: process.env.OPFLOW_LAB_URI || 'amqp://localhost',
+          subscriberName: 'app-harekeeper-subscriber',
+          recyclebinName: 'app-harekeeper-recyclebin',
+          applicationId: 'appHarekeeperLab'
+        }
+      }
     },
     appWebweaver: {
       defaultRedirectUrl: '/index'
